25a39a01eb0f410019e8bc3bf045c18f93326bbd
carefulRabbit <85527588+carefulRabbit@users.noreply.github.com>
2022-01-19 20:23:29 -0500

    Logo and Header Commit
    Added logo
    Created simple header
    Added responsive breakpoints for header styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import "bootstrap/dist/css/bootstrap.css";
 import Dictionary from "./Dictionary";
 import "./App.css";
 
+
+
 function App() {
   return (
-    <div className="container">
-      <header className="App-header"></header>
-      <main>
-        <Dictionary defaultKeyword="forest" />
-      </main>
-      <footer className="text-center">
-        Coded by{" "}
-        <a href="https://github.com/carefulRabbit/dictionarys-app" rel="noreferrer">Emily</a>{" "}
-      </footer>
+    <div >
+      <header className="App-header">
+        <img src="/images/logo.png" alt="Logo" className="logo" />
+        <p className="big-paragraph">Dictionary App</p>
+      </header>
+      <div className="container">
+        <main>
+          <Dictionary defaultKeyword="forest" />
+        </main>
+        <footer className="text-center">
+          Coded by{" "}
+          <a
+            href="https://github.com/carefulRabbit/dictionarys-app"
+            rel="noreferrer"
+          >
+            Emily
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
